aa2ace89f25c987f9d7c8cbfe49e5e20a91f8b7b
Altair <115722320+Altairu@users.noreply.github.com>
2023-08-23 23:50:55 +0900

    Update Altairu_Controller_get_V2.ino

--- 制御/Altairu_Controller_get_V2/Altairu_Controller_get_V2.ino
+++ 制御/Altairu_Controller_get_V2/Altairu_Controller_get_V2.ino
@@ -11,24 +11,35 @@ Motor mtr[3];
 Servo servo1;
 Servo servo2;
 
-const int S1 = 19; 
-const int S2 = 18;  
-int speed1,speed2,speed3;
+const int S1 = 19;
+const int S2 = 18;
+int speed1, speed2, speed3;
 
-bool databox[11]; 
+bool databox[11];
+bool limitSwitches[6];
+const int limitSwitchPins[] = { 27, 26, 25, 33, 32, 35, 34 };  // io27, io26, io25, io33, io32, io35, io34
+
+const float thresholdVoltage = 2.7;    // 閾値電圧
 
 void setup() {
   Serial.begin(115200);
   SerialBT.begin("ESP32test");
   Serial.println("device start");
-  // servo1.attach(S1, 510, 2400);  // 出力のピン, 最小値(usの値), 最大値（usの値）
-  // servo2.attach(S2, 510, 2400);
+  servo1.attach(S1, 510, 2400);
+  servo2.attach(S2, 510, 2400);
   mtr[0].init(23, 21, 22, 0);
   mtr[1].init(4, 2, 0, 1);
   mtr[2].init(13, 14, 12, 2);
+
+  for (int i = 0; i < 7; i++) {
+    pinMode(limitSwitchPins[i], INPUT);
+  }
 }
 
 void loop() {
+  readLimitSwitches();
+  updateSpeeds();
+
   mtr[0].ugoki(speed1);
   mtr[1].ugoki(speed2);
   mtr[2].ugoki(speed3);
@@ -37,17 +48,17 @@ void loop() {
 
 void mt() {
   if (SerialBT.available()) {
-    String receivedData = SerialBT.readStringUntil('\n');  // 改行まで
-    receivedData.trim();                                   // 文字列の前後の空白を取り除く
-    // カンマで分割
+    String receivedData = SerialBT.readStringUntil('\n');
+    receivedData.trim();
     int dataIndex = 0;
-    String data[11]; 
+    String data[11];
+
     while (receivedData.length() > 0 && dataIndex < 11) {
       int commaIndex = receivedData.indexOf(',');
       if (commaIndex != -1) {
         data[dataIndex] = receivedData.substring(0, commaIndex);
         receivedData = receivedData.substring(commaIndex + 1);
-        data[dataIndex].trim();  // 空白取り除く
+        data[dataIndex].trim();
         dataIndex++;
       } else {
         break;
@@ -61,8 +72,35 @@ void mt() {
       Serial.print(", ");
     }
     Serial.println();
-    speed1 = data[0] == "1" ? 255 : (data[2] == "1" ? -255 : 0);
-    speed2 = data[1] == "1" ? 255 : (data[3] == "1" ? -255 : 0);
-    speed3 = data[4] == "1" ? 255 : (data[6] == "1" ? -255 : 0);
+    speed1 = data[0] == "1" ? 256 : (data[2] == "1" ? -256 : 0);
+    speed2 = data[1] == "1" ? 256 : (data[3] == "1" ? -256 : 0);
+    speed3 = data[4] == "1" ? 256 : (data[6] == "1" ? -256 : 0);
+  }
+}
+
+void readLimitSwitches() {
+  for (int i = 0; i < 6; i++) {
+    limitSwitches[i] = analogRead(limitSwitchPins[i]) < (thresholdVoltage / 3.3) * 4095;
+  }
+}
+
+void updateSpeeds() {
+  if (limitSwitches[0]) {
+    speed1 = -256;
+  } 
+  if (limitSwitches[1]) {
+    speed2 = -256;
+  }
+  if (limitSwitches[2]) {
+    speed2 = -256;
+  }
+  if (limitSwitches[3]) {
+    speed2 = 256;
+  }
+  if (limitSwitches[4]) {
+    speed2 = -256;
+  }
+  if (limitSwitches[5]) {
+    speed3 = 256;
   }
 }
