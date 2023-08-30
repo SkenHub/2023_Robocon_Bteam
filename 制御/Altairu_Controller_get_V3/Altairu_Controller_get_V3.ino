ff6cee75475ec53a3137d28b5240d91545116ad8
hotaka <96649874+hotaka-mt@users.noreply.github.com>
2023-08-31 06:56:16 +0900

    get_V3のパラメータ調整

--- 制御/Altairu_Controller_get_V3/Altairu_Controller_get_V3.ino
+++ 制御/Altairu_Controller_get_V3/Altairu_Controller_get_V3.ino
@@ -11,17 +11,17 @@ Motor mtr[3];
 Servo servo1;
 Servo servo2;
 
-const int S1 = 19;
-const int S2 = 18;
+const int S1 = 18;
+const int S2 = 19;
 int speed1, speed2, speed3;
 double spk1 = 0.5;  //スピードゲイン
 double spk2 = 0.3;
-double spk3 = 0.5;
+double spk3 = 0.3;
 
 String data[11];
 bool databox[11];
 bool limitSwitches[6];
-const int limitSwitchPins[] = { 27, 26, 25, 33, 32, 35, 34 };  // io27, io26, io25, io33, io32, io35, io34
+const int limitSwitchPins[] = { 27, 26, 33, 25, 32, 35, 34 };  // io27, io26, io25, io33, io32, io35, io34
 
 const float thresholdVoltage = 2.7;  // 閾値電圧
 
@@ -31,8 +31,8 @@ void setup() {
   Serial.println("device start");
   servo1.attach(S1, 510, 2400);
   servo2.attach(S2, 510, 2400);
-  mtr[0].init(23, 21, 22, 0);
-  mtr[1].init(4, 2, 0, 1);
+  mtr[0].init(23, 21, 22, 3);
+  mtr[1].init(4, 2, 0, 4);
   mtr[2].init(13, 14, 12, 2);
 
   for (int i = 0; i < 7; i++) {
@@ -45,7 +45,14 @@ void loop() {
   updateSpeeds();
   MtrWrite(speed1,speed2,speed3);
   RaMt();
-  servomt();
+  //servomt();
+
+  Serial.print(speed1);
+  Serial.print("  ");
+  Serial.print(speed2);
+  Serial.print("  ");
+  Serial.print(speed3);
+  Serial.println("  ");
 
 /*
   for(int i=0; i<7; i++){
@@ -57,9 +64,9 @@ void loop() {
 }
 
 void MtrWrite(int mtr1,int mtr2,int mtr3){
-  mtr[0].ugoki(mtr1);
-  mtr[1].ugoki(mtr2);
-  mtr[2].ugoki(mtr3);
+  (mtr1!=0)? mtr[0].ugoki(mtr1) : mtr[0].tomaru();
+  (mtr2!=0)? mtr[1].ugoki(mtr2) : mtr[1].tomaru();
+  (mtr3!=0)? mtr[2].ugoki(mtr3) : mtr[2].tomaru();
 }
 void RaMt() {
   if (SerialBT.available()) {
@@ -79,6 +86,7 @@ void RaMt() {
       }
     }
 
+  /*
     for (int i = 0; i < dataIndex; i++) {
       Serial.print(i);
       Serial.print(": ");
@@ -86,14 +94,15 @@ void RaMt() {
       Serial.print(", ");
     }
     Serial.println();
+    */
   }
 }
 
 void servomt() {
   if (data[8] == "1") {
-    servo1.write(0);
+    //servo1.write(0);
   } else if (data[8] == "0") {
-    servo1.write(90);
+    //servo1.write(90);
   }
   if (data[9] == "1") {
     mtr[1].tomaru();
@@ -110,21 +119,22 @@ void readLimitSwitches() {
 }
 
 void updateSpeeds() {
-  if(data[0] == "1" && data[2] == "0"){
-    speed1 =  255 * spk1 * (limitSwitches[0])?1:0;
-  }else if(data[0] == "0" && data[2] == "1"){
-    speed1 = -255 * spk1 * (limitSwitches[1])?1:0;
+  //ウォーム速度
+  if(data[0] == "1" && data[2] == "0" && limitSwitches[0]){
+    speed1 =  255 * spk1;
+  }else if(data[0] == "0" && data[2] == "1" && limitSwitches[1] && !limitSwitches[3]){
+    speed1 = -255 * spk1;
   }else{ speed1 = 0; }
-  
-  if(data[1] == "1" && data[3] == "0"){
-    speed2 =  255 * spk2 * (limitSwitches[2])?1:0;
-  }else if(data[1] == "0" && data[3] == "1"){
-    speed2 = -255 * spk2 * (limitSwitches[3])?1:0;
+  //ロジャー速度
+  if(data[1] == "1" && data[3] == "0" && limitSwitches[2] && !limitSwitches[0]){
+    speed2 = -255 * spk2;
+  }else if(data[1] == "0" && data[3] == "1" && limitSwitches[3]){
+    speed2 =  255 * spk2*0.2;
   }else{ speed2 = 0; }
-
-  if(data[4] == "1" && data[6] == "0"){
-    speed3 =  255 * spk2 * (limitSwitches[4])?1:0;
-  }else if(data[4] == "0" && data[6] == "1"){
+  //お助け速度
+  if(data[4] == "1" && data[6] == "0" && limitSwitches[4]){
     speed3 = -255 * spk2;
+  }else if(data[4] == "0" && data[6] == "1"){
+    speed3 =  255 * spk2;
   }else{ speed3 = 0; }
 }
